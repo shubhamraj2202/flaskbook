2d54b7342cf960f44c222c931f410a9f10f3844e
Shubham Raj <shubhamraj2202@gmail.com>
2020-05-24 22:37:45 +0530

    Create/Register User Blueprint, @shubhamraj2202

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from user.views import user_app
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
+    app.register_blueprint(user_app)
+    # Above code->Flask to know new module/blueprint is created & make it available across other blueprints.
     return app
